aecaacb02c48d3705009c81e92a21c363968fe79
unknown <sanjaygiri50702@gmail.com>
2019-01-23 14:56:40 +0545

    coditional

---
+++ script.js
@@ -0,0 +1,16 @@
+// var num = prompt('enter your marks');
+// var passMarks = prompt('enter pass mark');
+// if(num > passMarks){
+//     alert('you are pass');
+// }
+// else{
+//     alert('you are fail');
+// }
+var bankAmount = prompt('enter your bank amount');
+var mblPrice = prompt('enter mobile price');
+if(mblPrice > bankAmount){
+    alert('cannot afford');
+}
+else{
+    alert('can afford');
+}
